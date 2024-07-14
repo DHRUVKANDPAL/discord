b3df244cea5f370b0b95b007ca48e30db7c8449a
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-07-14 17:24:11 +0530

    sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import {
 } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import Head from "next/head";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning={true}>
         <body className={cn(font.className,
           "bg-white dark:bg-[#313338]"
         )}>
